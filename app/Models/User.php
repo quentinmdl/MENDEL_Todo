6f661d48f2fcc76a6c10c64cf3aa9a0a0768596a
Nicolas Faessel <nicolas.faessel@ynov.com>
2020-11-20 16:23:13 +0100

    Auth ok, User KO

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,82 +6,17 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
-    /* Relations Eloquent */
-
-    /**
-     * Renvoi la liste des boards appartenant à l'utilisateur 
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMAny
-     */
-    public function ownedBoards()
-    {
-        return $this->hasMany(Board::class);
-    }
-
-    /**
-     * Renvoi la liste des boards auxquel l'utilisateur participe
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function boards()
-    {
-        return $this->belongsToMany(Board::class)
-                    ->using(BoardUser::class)
-                    ->withPivot("id")
-                    ->withTimestamps()
-                    ;
-    }
-
-    /**
-     * Renvoi la liste des toutes les tâches des boards auxquel l'utilisateur participe
-     * (hormis celles du board dont il est le propriétaire! )
-     * //TODO : test
-     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
-     */
-    public function allTasks() {
-        return $this->hasManyThrough(Task::class, BoardUser::class, 'user_id', 'board_id', 'id', 'board_id');
-    }
-
-
-    /**
-     * Renvoi la liste des tâches assignées à l'utilisateur
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function assignedTasks()
-    {
-        return $this->belongsToMany('App\Models\Task')
-                    ->using("App\Models\TaskUser")
-                    ->withPivot("id")
-                    ->withTimestamps();
-    }
-
-
-    /**
-     * Renvoi la liste des commentaires rédigés par l'utilisateur
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function comments()
-    {
-        return $this->hasMany('App\Models\Comment');
-    }
-
-    /**
-     * Renvoi la liste des pièces jointes posées par l'utilisateur
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function attachments()
-    {
-        return $this->hasMany('App\Models\Attachment');
-    }
-
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -102,6 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -111,5 +48,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 }
